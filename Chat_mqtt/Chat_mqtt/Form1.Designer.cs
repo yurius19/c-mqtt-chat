a64751c49daf5c8342f29a7e86323e6c5bbdded2
marcopedrinazzi <marcopedrinazzi.mp@gmail.com>
2018-01-10 22:41:25 +0100

    Modifica UI

--- Chat_mqtt/Chat_mqtt/Form1.Designer.cs
+++ Chat_mqtt/Chat_mqtt/Form1.Designer.cs
@@ -35,14 +35,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(694, 485);
+            this.button2.Location = new System.Drawing.Point(463, 315);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 29);
+            this.button2.Size = new System.Drawing.Size(67, 20);
             this.button2.TabIndex = 1;
             this.button2.Text = "Invia";
             this.button2.UseVisualStyleBackColor = true;
@@ -50,68 +50,66 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Location = new System.Drawing.Point(8, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(740, 438);
+            this.textBox1.Size = new System.Drawing.Size(522, 286);
             this.textBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 488);
+            this.label1.Location = new System.Drawing.Point(8, 317);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Your message:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(134, 485);
+            this.textBox2.Location = new System.Drawing.Point(89, 315);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(554, 29);
+            this.textBox2.Size = new System.Drawing.Size(371, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(8, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Nickname:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(100, 6);
+            this.textBox3.Location = new System.Drawing.Point(67, 4);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(162, 29);
+            this.textBox3.Size = new System.Drawing.Size(109, 20);
             this.textBox3.TabIndex = 7;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(268, 6);
+            this.button3.Location = new System.Drawing.Point(179, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(57, 29);
+            this.button3.Size = new System.Drawing.Size(55, 19);
             this.button3.TabIndex = 6;
             this.button3.Text = "OK";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // vScrollBar1
-            // 
-            this.vScrollBar1.Location = new System.Drawing.Point(754, 42);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(27, 438);
-            this.vScrollBar1.TabIndex = 8;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 525);
-            this.Controls.Add(this.vScrollBar1);
+            this.ClientSize = new System.Drawing.Size(543, 349);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
@@ -120,6 +118,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -136,7 +135,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
     }
 }
 
