158a0a603b005b006793023726122ec5b2fd89c2
yurius19 <33433456+yurius19@users.noreply.github.com>
2018-03-02 18:27:49 +0100

    a

---
+++ Chat_mqtt/Chat_mqtt/Chat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_mqtt
+{   
+    class Chat
+    {
+        string text;
+        Image img;
+        public Chat(string text, Image img)
+        {
+            this.text = text;
+            this.img = img;
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        public Image Image
+        {
+            get { return img; }
+            set { img = value; }
+        }
+
+        static internal List<Chat> get()
+        {
+            Chat item = new Chat("Benvenuto", null);
+            List<Chat> x = new List<Chat>();
+            return x;
+        }
+
+    }
+}
--- Chat_mqtt/Chat_mqtt/Form1.cs
+++ Chat_mqtt/Chat_mqtt/Form1.cs
@@ -24,17 +24,26 @@ namespace Chat_mqtt
         String nick;
         //System.DateTime moment = new System.DateTime(); inutile?
         Image img;
+        int flagTxtOrImage = 0;
 
         public Form1()
         {
             InitializeComponent();
-            string BrokerAddress = "127.0.0.1";
+            string BrokerAddress = "test.mosquitto.org";
 
             client = new MqttClient(BrokerAddress);
 
             client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(EventPublished);
-            RichTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            //RichTextBox1.BackColor = System.Drawing.SystemColors.Window;
             Bdisconnect.Enabled = false;
+
+            listView1.View = View.Details;
+            //listView1.GridLines = true;
+            //listView1.FullRowSelect = true;
+
+            //Add column header
+            listView1.Columns.Add("Chat", listView1.Width);
+
 
         }
 
@@ -42,14 +51,15 @@ namespace Chat_mqtt
         {
             try
             {
-                //SetText("*** Received Message");
-                //SetText("*** Topic: " + e.Topic);
-                String msg = System.Text.UTF8Encoding.UTF8.GetString(e.Message);
-                Char delimiter = '*';
-                String[] substrings = msg.Split(delimiter);
-                foreach (var substring in substrings)
-                    Console.WriteLine(substring);
-                SetText("[" + substrings[0] + "]" + substrings[1] +": "+substrings[2]);
+                    String msg = System.Text.UTF8Encoding.UTF8.GetString(e.Message);
+                    
+                    Char delimiter = '*';
+                    String[] substrings = msg.Split(delimiter);
+                    foreach (var substring in substrings)
+                        Console.WriteLine(substring);
+                    SetText("[" + substrings[0] + "]" + substrings[1] + ": " + substrings[2]);
+               
+                
             }
             catch (InvalidCastException ex)
             {
@@ -58,14 +68,17 @@ namespace Chat_mqtt
 
         private void SetText(string text)
         {
-            if (this.RichTextBox1.InvokeRequired)
+            if (this.listView1.InvokeRequired)
             {
                 SetTextCallback d = new SetTextCallback(SetText);
                 this.Invoke(d, new object[] { text });
             }
             else
             {
-                this.RichTextBox1.AppendText(text+"\n");
+                ListViewItem itm;
+                itm = new ListViewItem(text);
+                listView1.Items.Add(itm);
+                listView1.Items[listView1.Items.Count - 1].EnsureVisible();
                 //this.listChat.Items.Add(text);
             }
         }
@@ -81,6 +94,7 @@ namespace Chat_mqtt
                 client.Publish(Ttopic.Text, Encoding.UTF8.GetBytes(ora+'*'+Tnickname.Text+'*'+Tmessage.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
                 //listChat.Items.Add("*** Publishing on: " + Ttopic.Text);
                 Tmessage.Clear();
+                
             }
             catch (InvalidCastException ex)
             {
@@ -100,11 +114,15 @@ namespace Chat_mqtt
                 {
                     Tnickname.ReadOnly = true;
                     Ttopic.ReadOnly = true;
-                    RichTextBox1.AppendText("* Client connected\n");
+                    ListViewItem itm;
+                    itm = new ListViewItem("* Client connected\n");
+                    listView1.Items.Add(itm);
                     client.Subscribe(new string[] { Ttopic.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-                    RichTextBox1.AppendText("** Subscribing to: " + Ttopic.Text+"\n");
+                    itm = new ListViewItem(" * *Subscribing to: " + Ttopic.Text+"\n");
+                    listView1.Items.Add(itm);
                     Bconnect.Enabled = false;
                     Bdisconnect.Enabled = true;
+                    listView1.Items[listView1.Items.Count - 1].EnsureVisible();
                 }
                 else
                 {
@@ -120,23 +138,49 @@ namespace Chat_mqtt
         private void Bdisconnect_Click(object sender, EventArgs e)
         {
             client.Disconnect();
-            RichTextBox1.AppendText("*Client disconnected\n");
+            ListViewItem itm = new ListViewItem(" * Client disconnected\n");
+            listView1.Items.Add(itm);
             Tnickname.ReadOnly = false;
             Ttopic.ReadOnly = false;
             Bconnect.Enabled = true;
             Bdisconnect.Enabled = false;
+            listView1.Items[listView1.Items.Count - 1].EnsureVisible();
 
         }
 
         private void b_allega_Click(object sender, EventArgs e)
         {
-            string path;
+            string path, base64String;
             OpenFileDialog file = new OpenFileDialog();
             if (file.ShowDialog() == DialogResult.OK)
             {
                 path = file.FileName;
-                Image image = Image.FromFile(path);
-                Clipboard.SetDataObject(image);
+                /* using (Image image = Image.FromFile(path))
+                 {
+                     using (MemoryStream m = new MemoryStream())
+                     {
+                         image.Save(m, image.RawFormat);
+                         byte[] imageBytes = m.ToArray();
+
+                         // Convert byte[] to Base64 String
+                         base64String = Convert.ToBase64String(imageBytes);
+                         Console.WriteLine(base64String);
+                         //client.Publish(Ttopic.Text, Encoding.UTF8.GetBytes(base64String), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+                     }
+                 }*/
+                ImageList Imagelist = new ImageList();
+                Imagelist.Images.Add(Image.FromFile(path));
+                Imagelist.ImageSize = new Size(128, 128);
+                Imagelist.ColorDepth = ColorDepth.Depth32Bit;
+
+               
+                listView1.LargeImageList = Imagelist;
+                listView1.SmallImageList = Imagelist;
+                Random rnd = new Random();
+                int month = rnd.Next(1, 13);
+                listView1.Items.Add(new ListViewItem { ImageIndex = 0, Text = "Image "+month });
+                listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+                /*Clipboard.SetDataObject(image);
                 //Clipboard.SetImage(image);
                 RichTextBox1.AppendText(path+"\n");
                 DataFormats.Format dataFormat = DataFormats.GetFormat(DataFormats.Bitmap);
@@ -148,7 +192,7 @@ namespace Chat_mqtt
                     
                 }
                 else
-                    RichTextBox1.AppendText("niente da fare\n");
+                    RichTextBox1.AppendText("niente da fare\n");*/
                 //RichTextBox1.Paste();
                 //RichTextBox1.AppendText("\n");
                 //listView.Items.Add(path);
@@ -158,14 +202,14 @@ namespace Chat_mqtt
             }
 
         }
-        private byte[] Convertitore(System.Drawing.Image imageIn)
+        /*private byte[] Convertitore(System.Drawing.Image imageIn)
         {
             using (var ms = new MemoryStream())
             {
                 imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
                 return ms.ToArray();
             }
-        }
+        }*/
 
     }
     
